d7aca9094c710236ba5771b6b14b833e47a12dc8
Pablo Gomes <pablojr17@gmail.com>
2021-08-01 08:14:43 -0300

    🍀 title dinamico

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,9 +1,15 @@
+import Head from 'next/head'
 import styles from '../styles/home.module.scss'
 
 export default function Home() {
   return (
-    <div>
-      <h1 className={styles.title}>Projeto Next</h1>
-    </div>
+    <>
+      <Head>
+        <title>Board - Organizando suas tarefas</title>
+      </Head>
+      <div>
+        <h1 className={styles.title}>Projeto Next</h1>
+      </div>
+    </>
   )
 }
