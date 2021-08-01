28644a67d985dde8e8bff327bd352d628e51aad0
Pablo Gomes <pablojr17@gmail.com>
2021-08-01 08:10:01 -0300

    🐁 Create _document

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -3,7 +3,7 @@ import styles from '../styles/home.module.scss'
 export default function Home() {
   return (
     <div>
-      <h1 className={styles}>Projeto Next</h1>
+      <h1 className={styles.title}>Projeto Next</h1>
     </div>
   )
 }
