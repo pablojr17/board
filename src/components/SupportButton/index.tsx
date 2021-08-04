bcd9e8b09259e128423f49e48546f05e2172e791
Pablo Gomes <pablojr17@gmail.com>
2021-08-04 13:39:51 -0300

    Botão seja apoiador

---
+++ src/components/SupportButton/index.tsx
@@ -0,0 +1,13 @@
+/* eslint-disable @next/next/link-passhref */
+import Link from 'next/link';
+import styles from './styles.module.scss';
+
+export function SupportButton() {
+  return (
+    <div className={styles.donateContainer}>
+      <Link href="/donate">
+        <button>Apoiar</button>
+      </Link>
+    </div>
+  )
+}
--- src/pages/board/index.tsx
+++ src/pages/board/index.tsx
@@ -1,5 +1,6 @@
 import Head from 'next/head'
 import { FiCalendar, FiClock, FiEdit2, FiPlus, FiTrash } from 'react-icons/fi'
+import { SupportButton } from '../../components/SupportButton'
 import styles from './styles.module.scss'
 
 export default function Board() {
@@ -50,6 +51,8 @@ export default function Board() {
           <time>Ultima doação foi a 3 dias</time>
         </div>
       </div>
+
+      <SupportButton />
     </>
   )
 }
