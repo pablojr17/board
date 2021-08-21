0781597928952e940cbbfe7d1c76547df521a59d
Pablo Gomes <pablojr17@gmail.com>
2021-08-21 17:34:11 -0300

    page details

---
+++ src/pages/board/[id].tsx
@@ -0,0 +1,64 @@
+import {format} from "date-fns";
+import firebase from "../../services/firebaseConnection";
+import { GetServerSideProps } from "next";
+import { getSession } from "next-auth/client";
+
+type Task = {
+  id: string;
+  created: string | Date;
+  createdFormatted?: string;
+  tarefa: string;
+  userId: string;
+  nome: string;
+};
+
+interface TaskListProps {
+  data: string;
+}
+
+export default function Taks({data}: TaskListProps) {
+  const task = JSON.parse(data) as Task;
+
+  return (
+    <div>
+      <h1>Pagina detalhes</h1>
+      <p>{task.tarefa}</p>
+    </div>
+  );
+}
+
+export const getServerSideProps: GetServerSideProps = async ({req, params}) => {
+  const { id } = params;
+  const session = await getSession({req});
+
+  if(!session?.id) {
+    return {
+      redirect: {
+        destination: '/board',
+        permanent: false,
+      }
+    }
+  }
+
+  const data = await firebase.firestore().collection('tarefas')
+    .doc(String(id))
+    .get()
+    .then((snapshot) => {
+      const data = {
+        id: snapshot.id,
+        created: snapshot.data().created,
+        createdFormated: format(snapshot.data().created.toDate(), 'dd MMMM yyyy'),
+        tarefa: snapshot.data().tarefa,
+        userId: snapshot.data().userId,
+        nome: snapshot.data().nome,
+      }
+
+      return JSON.stringify(data);
+    })
+
+  return {
+    props: {
+      data
+    }
+  }
+}
